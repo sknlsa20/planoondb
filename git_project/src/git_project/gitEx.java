360de6d650276d0ca739fd17b2ac5b17f66ad691
sakoka <sakoka@192.168.155.19>
2022-10-17 16:17:58 +0530

    push file

---
+++ git_project/src/git_project/gitEx.java
@@ -0,0 +1,10 @@
+package git_project;
+
+public class gitEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git project uploaded");
+	}
+
+}
